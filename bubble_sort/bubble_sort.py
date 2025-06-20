95e43b0e8374cad18a92d0596e30ef9bd6e8f86a
Eric Ghati <ericghati@gmail.com>
2025-06-20 11:50:43 +0300

    bubble selection sorts

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,32 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+
+    #loop to iterate through the list of n number of elements.
+    for n in range(len(unsorted_list) - 1, 0, -1):
+
+        #we initialize a swapped to track and ensure the swaps occur as we organize the list
+        swapped = False
+
+        #loop to compare adjacent elements
+        for i in range(n):
+            if unsorted_list[i] > unsorted_list[i + 1]:
+
+                #swapping the elements if they are in the wrong order
+                (unsorted_list[i], unsorted_list[i+1]) = (unsorted_list[i+1], unsorted_list[i])
+
+                #track that a swap has happened
+                swapped = True
+
+            #if no swapps happened, the list is already sorted
+        if not swapped:
+            break
+
+                
+    return unsorted_list 
+
+#Testing with a sample list
+unsorted_list = [8,5,6]   
+bubble_sort(unsorted_list)
+print(f"Sorted list: {unsorted_list}")
+
+
+
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,22 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+
+    size =  len(arr)
+
+    for i in range(size):
+         min_index = i
+
+         for num  in range(i + 1, size):
+              #select the minimum element in each iteration
+              if arr[num] < arr[min_index]:
+                   min_index = num
+
+            #swapping  the elements to sort the array
+         (arr[i], arr[min_index]) = (arr[min_index], arr[i])
+
+arr = [-5, 6, 77, 606, -34, 90]
+selection_sort(arr)
+print(f"Sorted array: {arr}")
+    
+
+    
+    
